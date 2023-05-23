8ddcaa9b7a536d184cc7851f5d5b5d78e36499b7
Matthew Phan <mttphan261@gmail.com>
2023-05-23 12:16:27 -0500

    complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
+
 
 def batch_badge_creator(names):
-    return None
+    badge_list = []
+    for name in names:
+        badge_list.append(f'Hello, my name is {name}.')
+    return badge_list
+
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    index = 0
+    for name in names:
+        index += 1
+        room_assignments.append(
+            f"Hello, {name}! You'll be assigned to room {index}!")
+    return room_assignments
+
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+    
+    for room in assign_rooms(names):
+        print(room)
